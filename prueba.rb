ee5c7fe2b893e7141df749a056914f987c769694
Sainfuw <sainfuw@aol.com>
2018-06-19 14:14:22 -0400

    ADD: Base

---
+++ prueba.rb
@@ -0,0 +1,22 @@
+opt = 0
+while opt != 6
+  puts "MENU\n*****************************"
+  puts '1.- Promedio por alumno'
+  puts '2.- Inasistencias por alumno'
+  puts '3.- Alumnos aprobados'
+  puts '4.- Salir del programa'
+  print 'Ingrese una opcion: '
+  opt = gets.chomp.to_i
+  case opt
+  when 1
+    puts 'hola'
+  when 2
+    puts 'hola'
+  when 3
+    puts 'hola'
+  when 4
+    puts 'Programa terminado'
+  else
+    puts 'Debe Ingresar una opcion valida'
+  end
+end
